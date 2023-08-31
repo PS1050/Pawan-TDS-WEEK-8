e5dcd788337226a599ce6ecee5b809e0d86713c0
PS1050 <143725854+PS1050@users.noreply.github.com>
2023-08-31 20:15:29 +0530

    Update TDS W8.py

--- TDS W8.py
+++ TDS W8.py
@@ -1,24 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import streamlit as st
 
-def find_largest(num1, num2, num3):
-    return max(num1, num2, num3)
 
 def main():
-    st.title("Find the Largest Number")
+    st.title("Find the Largest Number among 3 numbers")
     
-    num1 = st.number_input("Enter the first number:")
-    num2 = st.number_input("Enter the second number:")
-    num3 = st.number_input("Enter the third number:")
+    x = st.number_input("Enter the first number:")
+    y = st.number_input("Enter the second number:")
+    z = st.number_input("Enter the third number:")
     
     if st.button("Find Largest"):
-        largest = find_largest(num1, num2, num3)
+        largest = max(x, y, z)
         st.write(f"The largest number is: {largest}")
+
 
 if __name__ == "__main__":
     main()
